2b571e842e6aa390edc2925fa7eabf35d9e2a370
vanessa2493 <56240421+vanessa2493@users.noreply.github.com>
2022-07-29 21:51:30 -0300

    category table initiated

---
+++ src/pages/AddCategory/Table/index.tsx
@@ -0,0 +1,22 @@
+import { getCategories } from "../../../api"
+import { data } from "../../../types"
+
+const Table = () => {
+    
+    {
+        const lista = async () => {
+                const data: data[] = await getCategories()
+                data.map(({name, slug, idDB}) =>{
+                    return(
+                        <div>
+                            <span>{name}</span>
+                            <a href="">Eliminar</a>
+                            <a href={`./edit-category:${slug}`}>Editar</a>
+                        </div>
+                    )
+                }
+            )
+        }
+        
+    }
+}
